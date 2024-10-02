5a18c4aeae0ca24bce7cf6ea7753d25605f154f5
Rares Otet <ro436@live.mdx.ac.uk>
2024-10-02 12:12:18 +0100

    Importing files to remote

---
+++ script.js
@@ -0,0 +1,20 @@
+let webstore = new Vue({
+    el: '#booking',
+    data: {
+        titlename: 'Booking Lessons',
+        lesson1: {
+            image: 'Images/music.png',
+            title: 'Music Lesson',
+            description: 'blah blah',
+            price: 25,
+            availability: 5
+        },
+        lesson2: {
+            image: 'Images/maths.png',
+            title: 'Maths Lesson',
+            description: 'blah blah',
+            price: 20,
+            availability: 5
+        }
+    }
+});
